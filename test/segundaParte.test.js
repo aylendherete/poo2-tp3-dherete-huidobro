b4f1b0d473338f36ac1e025d2989f83f777100a2
aylendherete <114444320+aylendherete@users.noreply.github.com>
2022-10-24 22:42:23 -0300

    TDD iteration

--- test/segundaParte.test.js
+++ test/segundaParte.test.js
@@ -3,6 +3,10 @@ const { default: expect } = require('expect');
 const Posicion=require('../main/Posicion.js');
 const Tablero=require('../main/Tablero.js');
 
+beforeEach(function(){
+    var tablero=new Tablero();
+    tablero.crearTablero();
+})
 
 test("Encender",()=>{
     var tablero=new Tablero();
@@ -20,3 +24,20 @@ test("Encender",()=>{
     expect(tablero.tablero[2,1].estadoActual()).toBe(1);
     expect(tablero.tablero[2,2].estadoActual()).toBe(1);
 })
+
+test("Apagar",()=>{
+    var posicionSuperior=new Posicion(1,0);
+    var posicionInferior= new Posicion(2,2);
+    tablero.encender(posicionSuperior,posicionInferior);
+    var posicionSup2=new Posicion(1,1);
+    var posocionInf2=new Posicion(2,2);
+    tablero.apagar(posicionSup2,posocionInf2);
+    expect(tablero.tablero[1,0].estadoActual()).toBe(1);
+    expect(tablero.tablero[1,1].estadoActual()).toBe(0);
+    expect(tablero.tablero[1,2].estadoActual()).toBe(0);
+    expect(tablero.tablero[2,0].estadoActual()).toBe(1);
+    expect(tablero.tablero[2,1].estadoActual()).toBe(0);
+    expect(tablero.tablero[2,2].estadoActual()).toBe(0);
+})
+
+
