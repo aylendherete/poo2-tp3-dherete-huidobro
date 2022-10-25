ebef4acf178bd95f83c511b403cfaf909ddb3643
aylendherete <114444320+aylendherete@users.noreply.github.com>
2022-10-24 22:32:41 -0300

    TDD iteration

--- test/segundaParte.test.js
+++ test/segundaParte.test.js
@@ -7,5 +7,6 @@ const Tablero=require('../main/Tablero.js');
 test("Encender",()=>{
     var tablero=new Tablero();
     tablero.crearTablero();
+    
     expect(tablero.tablero[0,1].estadoActual()).toBe(0);
 })
